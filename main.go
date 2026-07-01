8459547545fe33ce85b4584d21cf21951e8ef7eb
Lyubomir Ganev <luboganev@gmail.com>
2026-07-01 08:12:29 +0000

    implement initial repl loop

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+		if !scanner.Scan() {
+			break
+		}
+		rawInput := scanner.Text()
+		words := cleanInput(rawInput)
+		if len(words) > 0 {
+			fmt.Printf("Your command was: %s\n", words[0])
+		} else {
+			fmt.Print("Your command was empty\n")
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Print("Failed to scan input. Ending program.\n")
+	}
 }
