117b6d84e3bee9935d4ce7924a42aa8bb504c6f7
Lyubomir Ganev <luboganev@gmail.com>
2026-07-02 11:59:12 +0000

    add actual helpful commands

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex >")
-		if !scanner.Scan() {
-			break
-		}
-		rawInput := scanner.Text()
-		words := cleanInput(rawInput)
-		if len(words) > 0 {
-			fmt.Printf("Your command was: %s\n", words[0])
-		} else {
-			fmt.Print("Your command was empty\n")
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Print("Failed to scan input. Ending program.\n")
-	}
+	startRepl()
 }
